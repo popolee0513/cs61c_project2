9def7aecb9de11873d4ca06722172cd2a6b08b93
peterlee <peterlee@mobagel.com>
2025-06-03 14:47:10 +0800

    finish read_matrix

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -24,19 +24,98 @@
 # - If you receive an fclose error or eof,
 #   this function terminates the program with error code 92.
 # ==============================================================================
+
 read_matrix:
-
     # Prologue
-	
+    addi sp, sp, -28
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw ra, 24(sp)    
+    
+    mv s0, a0     # filename
+    mv s1, a1     # row_ptr
+    mv s2, a2     # col_ptr
 
+    # fopen
+    mv a1, s0      # filename
+    li a2, 0       # read mode
+    call fopen
+    mv s3, a0      # file descriptor
+    li t0, -1
+    beq a0, t0, fopen_error
 
+    # malloc 8 bytes for header
+    li a0, 8
+    call malloc
+    beq a0, x0, malloc_error
+    mv s4, a0
 
+    # fread 8 bytes
+    mv a1, s3
+    mv a2, s4
+    li a3, 8
+    call fread
+    li t1, 8
+    blt a0, t1, fread_error
+    lw t0, 0(s4)        
+    sw t0, 0(s1)          
+    lw t2, 4(s4)          
+    sw t2, 0(s2)    
 
+    # malloc matrix
+    mul t3, t0, t2        # total elements
+    li t4, 4
+    mul t6, t3, t4        # total bytes
+    mv a0, t6
+    call malloc
+    beq a0, x0, malloc_error
+    mv s5, a0             # matrix_ptr
 
+    # fread matrix data
+    mv a1, s3
+    mv a2, s5
+    mv a3, t6
+    call fread
+    blt a0, t6, fread_error
 
-
+    # fclose
+    mv a1, s3
+    call fclose
+    li t0, -1
+    beq a0, t0, fclose_error
 
     # Epilogue
-
-
+end:
+    mv a0, s5          # return matrix_ptr
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw ra, 24(sp)
+    addi sp, sp, 28
     ret
+
+
+# ==== Error handlers ====
+fopen_error:
+    li a1, 90
+    call exit2
+
+fread_error:
+    li a1, 91
+    call exit2
+
+malloc_error:
+    li a1, 88
+    call exit2
+
+fclose_error:
+    li a1, 92
+    call exit2
+
--- unittests/unittests.py
+++ unittests/unittests.py
@@ -148,14 +148,16 @@ class TestReadMatrix(TestCase):
         cols = t.array([-1])
 
         # load the addresses to the output parameters into the argument registers
-        raise NotImplementedError("TODO")
-        # TODO
+        t.input_array("a1", rows)
+        t.input_array("a2", cols)
 
         # call the read_matrix function
         t.call("read_matrix")
 
         # check the output from the function
-        # TODO
+        t.check_array(rows, [3])
+        t.check_array(cols, [3])
+        t.check_array_pointer("a0",[1,2,3,4,5,6,7,8,9])
 
         # generate assembly and run it through venus
         t.execute(fail=fail, code=code)
